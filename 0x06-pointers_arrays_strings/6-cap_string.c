ce58ac345673463c2447ea7f1088017bc6516a33
ThankG12 <thankgod1207@gmail.com>
2023-06-30 09:22:50 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,19 +15,19 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index -1] == ' ' ||
-		    str[index -1] == '\t' ||
-		    str[index -1] == '\n' ||
-		    str[index -1] == ',' ||
-		    str[index -1] == ';' ||
-		    str[index -1] == '.' ||
-		    str[index -1] == '!' ||
-		    str[index -1] == '?' ||
-		    str[index -1] == '"' ||
-		    str[index -1] == '(' ||
-		    str[index -1] == ')' ||
-		    str[index -1] == '{' ||
-		    str[index -1] == '}' ||
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
 
@@ -36,3 +36,4 @@ char *cap_string(char *str)
 
 	return (str);
 }
+
