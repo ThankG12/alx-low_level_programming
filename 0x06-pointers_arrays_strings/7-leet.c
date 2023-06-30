0ee623fe8df540808e0f1346b4eb15c55b837490
ThankG12 <thankgod1207@gmail.com>
2023-06-30 05:41:49 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,6 @@
 #include <main.h>
-#include <stdlib.h>
-#include <string.h>
 
 char* leet(char* input) {
-    char* encoded = malloc(strlen(input) + 1);  // Allocate memory for the encoded string
-    strcpy(encoded, input);  // Copy the input string to the encoded string
-    
     char replacements[5][2] = {
         {'a', '4'},
         {'e', '3'},
@@ -15,13 +10,16 @@ char* leet(char* input) {
     };
 
     for (int i = 0; i < 5; i++) {
-        for (int j = 0; encoded[j] != '\0'; j++) {
-            if (encoded[j] == replacements[i][0] || encoded[j] == replacements[i][0] - 'a' + 'A') {
-                encoded[j] = replacements[i][1];
+        char lower = replacements[i][0];
+        char upper = lower - 'a' + 'A';
+        char replace = replacements[i][1];
+
+        for (int j = 0; input[j] != '\0'; j++) {
+            if (input[j] == lower || input[j] == upper) {
+                input[j] = replace;
             }
         }
     }
 
-    return encoded;
+    return input;
 }
-
