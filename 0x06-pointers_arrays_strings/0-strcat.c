9883b9d0f187d70a2a0941e57c38659caec37dae
ThankG12 <thankgod1207@gmail.com>
2023-06-29 18:13:00 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,37 +1,29 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
 
-char* _strcat(char* dest, const char* src) {
-    char* ptr = dest;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    // Find the end of the destination string
-    while (*ptr != '\0') {
-        ptr++;
-    }
-
-    // Append characters from src to dest
-    while (*src != '\0') {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-
-    // Add the terminating null byte
-    *ptr = '\0';
-
-    return dest;
+	dest[i] = '\0';
+	return (dest);
 }
-
-int main() {
-    char dest[100] = "Hello, ";
-    const char* src = "world!";
-
-    printf("Before concatenation: %s\n", dest);
-
-    _strcat(dest, src);
-
-    printf("After concatenation: %s\n", dest);
-
-    return 0;
-}
-
